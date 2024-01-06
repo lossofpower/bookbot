ccbaaf779e56e262402a7fb40825fcbc80a0936c
lossofpower <thoturner@hotmail.com>
2024-01-06 01:24:17 -0600

    counting letters in frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+with open("books/frankenstein.txt") as f:
+    file_contents = f.read().lower()
+    words = file_contents.split()
+
+def returnLetters():
+    dict = {}
+    print("working")
+    for word in words:
+        for letter in word:
+            if letter in dict:
+                dict[letter] += 1
+            else:
+                dict[letter] = 1
+
+    print(dict)
+
+returnLetters()
