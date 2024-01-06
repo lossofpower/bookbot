0300f6f5a587c9ccc54193629a246b7329bd0fd5
lossofpower <thoturner@hotmail.com>
2024-01-06 14:22:07 -0600

    Returns a report of word and character count

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    returnLetters(text)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print()
+    print(f"{num_words} words found in the document")
+    print()
+    for item in chars_sorted_list:
+        print(f"The {item[0]} character was found {item[1]} times")
+    print()
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read().lower()
+    
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def returnLetters(words):
-    dict = {}
-    list = []
+def get_chars_dict(text):
+    chars = {}
     print("working")
-    for word in words:
+    for word in text:
         for letter in word:
-            if letter in dict:
-                dict[letter] += 1
+            if letter in chars:
+                chars[letter] += 1
             else:
-                dict[letter] = 1
+                chars[letter] = 1
+    return chars
 
-    # print(dict.items())
-    for k,v in dict.items():
+def chars_dict_to_sorted_list(chars_dict):
+    list = []
+    for k,v in chars_dict.items():
         temp = [k,v]
         if temp[0].isalpha():
             list.append(temp)
-    for item in list:
-        print(f"The {item[0]} character was found {item[1]} times")
+    list.sort()
+    return list
 
 
 main()
